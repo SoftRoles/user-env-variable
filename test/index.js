689bdf63590fb104e1729c9733f9264b3078e9ae
Hüseyin <yigit.hsyn@gmail.com>
2019-01-21 19:17:44 +0300

    assert added to test

--- test/index.js
+++ test/index.js
@@ -1,15 +1,15 @@
-
+var assert = require('assert')
 var userEnvVariable = require("../lib/index")
 
 userEnvVariable.list(function(err, result){
-  console.log(err == true)
+  assert.equal(err,null)
   console.log(result)
 })
 userEnvVariable.get('SOFTROLES_SERVICE_FILESYSTEM_PORT', function(err, result){
-  console.log(err == true)
+  assert.equal(err,null)
   console.log(result)
 })
 
 userEnvVariable.set('SOFTROLES_SERVICE_FILESYSTEM_PORT', 3004, function(err){
-  console.log(err)
+  assert.equal(err,null)
 })
