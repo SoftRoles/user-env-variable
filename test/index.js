b7bb3562cc61102e356680905b6294a17e84686d
Hüseyin <yigit.hsyn@gmail.com>
2019-01-22 13:57:32 +0300

    test uncommenct

--- test/index.js
+++ test/index.js
@@ -5,10 +5,10 @@ userEnvVariable.list(function(err, result){
   assert.equal(err,null)
   console.log(result)
 })
-// userEnvVariable.get('SOFTROLES_SERVICE_FILESYSTEM_PORT', function(err, result){
-//   assert.equal(err,null)
-//   console.log(result)
-// })
+userEnvVariable.get('SOFTROLES_SERVICE_FILESYSTEM_PORT', function(err, result){
+  assert.equal(err,null)
+  console.log(result)
+})
 
 // userEnvVariable.set('SOFTROLES_SERVICE_FILESYSTEM_PORT', 3004, function(err){
 //   assert.equal(err,null)
